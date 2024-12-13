e3f5febb95d932adc02fa178af3de5912ae13cb7
marcostsi <lira.santos@academico.ifpb.edu.br>
2024-12-13 14:03:48 +0000

    alterando a atividade

--- Tarefa01/script.js
+++ Tarefa01/script.js
@@ -1,21 +1,26 @@
-document.querySelector('#empurrar').onclick = function(){
-    if(document.querySelector('#container input').value.length == 0){
-        alert("Por favor insira uma tarefa")
-    }
-    else{
-        document.querySelector('tarefas').innerHTML += `
-    <div class="tarefas">
-        <span id="nomedatarefa">
-        ${document.querySelector('#container input').value}
-        </span>
-        <button class="delete"><i class="longe"></i></button>
-    </div>
-    `;
-    let tarefas_atual = document.querySelectorAll(".delete");
-    for(let i=0; i < tarefas_atual.length; i++){
-        tarefas_atual[i].onclick = function(){
-            this.parentNode.remove();
+document.querySelector('#empurrar').onclick = function () {
+    const input = document.querySelector('#novatarefa input');
+    const tarefasContainer = document.querySelector('#tarefas');
+
+    if (input.value.trim() === "") {
+        alert("Por favor insira uma tarefa");
+    } else {
+        tarefasContainer.innerHTML += `
+            <div class="tarefas">
+                <span id="nomedatarefa">
+                    ${input.value}
+                </span>
+                <button class="delete">
+                    <i class="fas fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+        input.value = "";
+        const tarefasAtual = document.querySelectorAll(".delete");
+        for (let i = 0; i < tarefasAtual.length; i++) {
+            tarefasAtual[i].onclick = function () {
+                this.parentNode.remove();
+            };
         }
     }
-    }
-}
+};
